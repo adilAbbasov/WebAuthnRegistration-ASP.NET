1b8bfe7fab1a3bd4c4ef8244bcb4e3ac8e6a3b26
Adil <abbasov.adil.n@gmail.com>
2024-02-25 16:16:49 +0400

    Updates on index and script files

--- script.js
+++ script.js
@@ -16,7 +16,25 @@ document.addEventListener('DOMContentLoaded', function ()
     {
         event.preventDefault();
 
-        var cridential = await getCredentials();
+        const userName = document.getElementById('loginUserName').value;
+        const credentialId = await confirmUserName(userName);
+
+        if (!credentialId)
+        {
+            document.getElementById('responseMessage').textContent = 'Login failed. User not found.';
+            console.error('Error:', error);
+            return;
+        }
+
+        const credential = await getCredentials(credentialId);
+
+        if (!credential)
+        {
+            document.getElementById('responseMessage').textContent = 'Login failed. Credentials not found.';
+            console.error('Error:', error);
+            return;
+        }
+
         const formData = new FormData(this);
         const jsonObject = {};
 
@@ -25,7 +43,7 @@ document.addEventListener('DOMContentLoaded', function ()
             jsonObject[key] = value;
         });
 
-        jsonObject["fingerprintKey"] = cridential.id;
+        jsonObject["fingerprintKey"] = credential.id;
         const jsonData = JSON.stringify(jsonObject);
 
         fetch('https://localhost:7244/controller/login', {
@@ -53,14 +71,24 @@ document.addEventListener('DOMContentLoaded', function ()
                 console.error('Error:', error);
             });
 
-        document.getElementById('loginUsername').value = '';
+        document.getElementById('loginUserName').value = '';
     });
 
     document.getElementById('signupForm').addEventListener('submit', async function (event)
     {
         event.preventDefault();
 
-        var cridential = await createCredentials();
+        var email = document.getElementById('email').value;
+        var userName = document.getElementById('signupUserName').value;
+        var credential = await createCredentials(email, userName);
+
+        if (!credential)
+        {
+            document.getElementById('responseMessage').textContent = 'Signup failed. Credentials not created.';
+            console.error('Error:', error);
+            return;
+        }
+
         const formData = new FormData(this);
         const jsonObject = {};
 
@@ -69,7 +97,7 @@ document.addEventListener('DOMContentLoaded', function ()
             jsonObject[key] = value;
         });
 
-        jsonObject["fingerprintKey"] = cridential.id;
+        jsonObject["fingerprintKey"] = credential.id;
         const jsonData = JSON.stringify(jsonObject);
 
         fetch('https://localhost:7244/controller/signup', {
@@ -81,7 +109,7 @@ document.addEventListener('DOMContentLoaded', function ()
         })
             .then(response =>
             {
-                document.getElementById('responseMessage').textContent = cridential.id;
+                document.getElementById('responseMessage').textContent = credential.id;
             })
             .catch(error =>
             {
@@ -89,11 +117,11 @@ document.addEventListener('DOMContentLoaded', function ()
                 console.error('Error:', error);
             });
 
-        document.getElementById('signupUsername').value = '';
+        document.getElementById('signupUserName').value = '';
         document.getElementById('email').value = '';
     });
 
-    async function createCredentials()
+    async function createCredentials(email, userName)
     {
         return new Promise((resolve, reject) =>
         {
@@ -102,19 +130,15 @@ document.addEventListener('DOMContentLoaded', function ()
                 navigator.credentials.create({
                     publicKey: {
                         rp: {
-                            id: "cosmosclownstore.com",
-                            name: "Cosmo’s Clown Store"
+                            name: "WebAuthnRegistration"
                         },
                         user: {
-                            id: "1234",
-                            name: "krusty@example.com",
-                            displayName: "Krusty The Clown"
+                            id: Date.now(),
+                            name: email,
+                            displayName: userName
                         },
-                        challenge: Uint8Array.from("some-random-bytes"),
+                        challenge: Uint8Array.from("MinA"),
                         pubKeyCredParams: [{ type: "public-key", alg: -7 }],
-                        authenticatorSelection: {
-                            authenticatorAttachment: "cross-platform",
-                        },
                     }
                 }).then(credential =>
                 {
@@ -136,19 +160,17 @@ document.addEventListener('DOMContentLoaded', function ()
         });
     }
 
-    async function getCredentials()
+    async function getCredentials(credentialId)
     {
         return new Promise((resolve, reject) =>
         {
             try
             {
-                const credentialId = "aaaaaaaaaaaaaaaaaa";
-
                 navigator.credentials.get({
                     publicKey: {
-                        rpId: "cosmosclownstore.com",
-                        challenge: Uint8Array.from("some-random-bytes"),
+                        rpName: "WebAuthnRegistration",
                         userVerification: "preferred",
+                        challenge: Uint8Array.from("MinA"),
                         allowCredentials: [{ type: "public-key", id: credentialId }]
                     }
                 }).then(credential =>
@@ -171,6 +193,33 @@ document.addEventListener('DOMContentLoaded', function ()
         });
     }
 
+    async function confirmUserName(userName)
+    {
+        try
+        {
+            const response = await fetch(`https://localhost:7244/controller/users/${userName}`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            });
+
+            if (response.ok)
+            {
+                const data = await response.json();
+                console.log("Fingerprint Key:", data);
+            } else if (response.status === 404)
+            {
+                console.log("User not found.");
+            } else
+            {
+                console.error("Failed to fetch data. Status code:", response.status);
+            }
+        } catch (error)
+        {
+            console.error('Error:', error);
+        }
+    }
 });
 
 
